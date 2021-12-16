7a316e179cbb7f6987a07e73abe6b3c321bd627a
Kenneth Flynn <flynnkennethd@gmail.com>
2021-12-16 02:02:42 -0500

    Finished the cipher with full functionality

---
+++ cipher.rb
@@ -0,0 +1,16 @@
+def caesar_cipher(string, key)
+  product = ""
+  string.each_char do |char|
+    #sets minimum values for caps and lowercase
+    min = char.ord < 91 ? 65 : 97 
+    #if to ignore punctuation
+    if char.ord.between?(65, 90) || char.ord.between?(97, 122)
+      changed_char = (char.ord - min + key) % 26 + min
+      product += changed_char.chr
+    else
+      product += char
+    end
+  end
+  #returns product
+  product
+end
